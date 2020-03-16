8f9341ea701787d3470935a7964234bc6829b40f
Lu Jianyi2 <18775397507@163.com>
2020-03-16 23:11:13 +0800

    update by the second developer

--- fileinfo.py
+++ fileinfo.py
@@ -28,3 +28,47 @@ for i in range(a,b+1):
     A=1
 c=int(sum(ls))
 print(c)
+
+######
+#质因数分解.py
+'''
+x=int(input())
+i=2
+a=''
+    
+while x%i==0 and x>=i:
+    
+    a=a+str(i)+' * '
+    x=x/i
+
+else:
+    i+=1
+    a=''
+    a=a+str(i)+' * '
+    x=x/i
+print(a)
+'''
+
+###质因数分解
+
+import math
+number=int(input())
+list=[]
+def getChildren(num):
+    print('*'*30)
+    isZhishu=True
+    i=2
+    square=int(math.sqrt(num))+1
+    while i<=square:
+        if num%i==0:
+            list.append(i)
+            isZhishu=False
+            getChildren(num/i)
+            i+=1
+            break
+        i+=1
+    if isZhishu:
+        list.append(num)
+getChildren(number)
+print(list)
+
